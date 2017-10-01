8eb961049471dbdb22402aba7740d26510074252
enactusvitvellore <31859643+enactusvitvellore@users.noreply.github.com>
2017-10-01 08:37:42 +0530

    beta

--- data.php
+++ data.php
@@ -1,6 +1,6 @@
 
 <?php
-if(isset($_POST["submit"])) {
+if ($_POST['submit'])  {
 $recipient = "kashyapshashank@live.com"; //my email
 echo $subject = 'Email message from Point Plumbing';
 echo $name = $_POST['name'];
@@ -10,9 +10,7 @@ echo $comment = $_POST['comment'];
 
  $mailBody="From: $name\n E-Mail: $email\n Phone:\n $phone\n Comment:\n $comment"; 
 
- mail($recipient, $subject, $mailBody, "From: $name <$email>");
-
-echo $thankYou="<p>Thank you! We will be in contact with you shortly.</p>";
+ mail($recipient, $subject, $mailBody, $email);
 
 }
 ?>
